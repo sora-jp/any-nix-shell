9c5e2cb4175b258ac2e9f30d494081c77019b1d3
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2024-01-10 15:06:48 +0100

    initial flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Flake providing any-nix-shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.any-nix-shell = pkgs.stdenv.mkDerivation rec {
+          pname = "any-nix-shell";
+          version = "1.2.1";
+          src = self;
+          strictDeps = true;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildInputs = [ pkgs.bash ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r bin $out
+            wrapProgram $out/bin/any-nix-shell --prefix PATH ":" $out/bin
+          '';
+
+          meta = with pkgs.lib; {
+            description = "fish and zsh support for nix-shell";
+            license = licenses.mit;
+            homepage = "https://github.com/haslersn/any-nix-shell";
+            maintainers = with maintainers; [ haslersn ];
+            mainProgram = "any-nix-shell";
+          };
+        };
+    });
+}
